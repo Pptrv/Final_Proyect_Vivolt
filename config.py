5cfcec0c902b6522758455c7a31e65c6eb044609
pptrv <trv.pepe@gmail.com>
2020-12-13 19:46:28 +0100

    domingo, atascado

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
+# Consumo Cliente
 login_data = {
     'name': 'vivolt',
     'password': 'temporal01vivolt'
 }
 client_cups = {'cups' : 'ES0021000005204736CV'}
 
+# Api: conectar la base de datos en SQL a VIsual Estudio y luego esconder en .env
